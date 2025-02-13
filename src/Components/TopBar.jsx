68b2f5e7d92ba63a2b0efb11e2f9a2717d2176a3
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-02-13 20:47:08 +0330

    feat : mountain pic added

--- src/Components/TopBar.jsx
+++ src/Components/TopBar.jsx
@@ -3,8 +3,18 @@ import React from 'react'
 export default function TopBar() {
   return (
     <div className='topBar_container'>
+        <div className='wrapper'>
 
-    
+            <div className="left_content">
+                <div className='img_container w-20 '>
+
+                    <img className='w-full h-full' src="/images/mountain-2.png" alt="mountain" />
+
+                </div>
+            </div>
+
+        </div>
+
 
     </div>
   )
