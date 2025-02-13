682c58a04ccb26410923252f9124eae94b685644
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-02-13 20:51:57 +0330

    react iconify installed

--- src/Components/TopBar.jsx
+++ src/Components/TopBar.jsx
@@ -3,14 +3,18 @@ import React from 'react'
 export default function TopBar() {
   return (
     <div className='topBar_container'>
-        <div className='wrapper'>
+        <div className='wrapper p-2'>
 
-            <div className="left_content">
+            <div className="left_content flex items-center">
                 <div className='img_container w-20 '>
 
                     <img className='w-full h-full' src="/images/mountain-2.png" alt="mountain" />
 
                 </div>
+                <h2 className="font-bold text-lg">Sahand</h2>
+            </div>
+            <div className="right_content">
+                
             </div>
 
         </div>
