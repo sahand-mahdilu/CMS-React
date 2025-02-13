7d7d25b4b6b7b6fd0227e1d7818882c84ab22388
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-02-13 20:05:17 +0330

    feat : users page added to routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router";
 import Home from "./Pages/Home/Home";
 import NewUser from "./Pages/NewUser/NewUser";
 import Products from "./Pages/Products/Products";
+import Users from "./Pages/Users/Users";
 
 let router= createBrowserRouter([
     {path:"/",element:<Home/>},
     {path:"/newUser",element:<NewUser/>},
     {path:"/products",element:<Products/>},
+    {path:"/users",element:<Users/>},
 
 
 ])
