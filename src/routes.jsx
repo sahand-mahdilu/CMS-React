1342829317524cf8b66a931a1fec52fd54e9c3ca
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-02-13 20:02:45 +0330

    feat NewUser Page adde to routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,8 +1,10 @@
 import { createBrowserRouter } from "react-router";
 import Home from "./Pages/Home/Home";
+import NewUser from "./Pages/NewUser/NewUser";
 
 let router= createBrowserRouter([
-    {path:"/",element:<Home/>}
+    {path:"/",element:<Home/>},
+    {path:"/newUser",element:<NewUser/>},
 
 
 ])
