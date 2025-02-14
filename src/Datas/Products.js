e87b017d8cdec7ad3aa1d7cef3b4e0986943b0d3
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-02-14 22:40:02 +0330

    feat : products component content added

--- src/Datas/Products.js
+++ src/Datas/Products.js
@@ -2,6 +2,8 @@ let productsData=[
     {id:1,name:"A52s", price:"102$",image:"./images/phone-1.jpg"},
     {id:2,name:"nokia", price:"202$",image:"./images/phone-2.jpg"},
     {id:3,name:"apple", price:"302$",image:"./images/phone-3.jpg"},
-    {id:3,name:"samsung", price:"322$",image:"./images/phone-4.png"},
-    {id:3,name:"HTC", price:"152$",image:"./images/phone-5.png"},
+    {id:4,name:"samsung", price:"322$",image:"./images/phone-4.png"},
+    {id:5,name:"HTC", price:"152$",image:"./images/phone-5.png"},
 ]
+
+export{productsData}
--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { productsData } from '../../Datas/Products'
+import { NavLink } from 'react-router'
 
 export default function Products() {
+
+  const [items,setItems]=useState((productsData))
+
+
+  const deleteHandler= (productId)=>{
+
+    setItems(items.filter(item=>{
+
+      return item.id !==productId
+    }))
+
+     
+
+    
+
+  }
+  
 
   
   return (
@@ -16,22 +35,22 @@ export default function Products() {
       </thead>
       <tbody>
 
-  {users.map(user=>{
+  {items.map(item=>{
     return(
 
-      <tr key={user.id} className=" ">
-      <td className="p-2 px-16">{user.id}</td>
+      <tr key={item.id} className=" ">
+      <td className="p-2 px-16">{item.id}</td>
 
       <td className="p-2 px-16">
         <div className="flex items-center gap-2">
           <div className="img-contaner size-12 rounded-full overflow-hidden">
             <img
               className="w-full h-full"
-              src="./images/profile.png"
+              src={item.image}
               alt=""
             />
           </div>
-          <span>{user.name}</span>
+          <span>{item.name}</span>
         </div>
       </td>
       <td>price</td>
@@ -39,10 +58,10 @@ export default function Products() {
     
       <td className="p-2 px-16">
         <div className="flex gap-2">
-        <NavLink to={`/mainUser/${user.id}`}>
+        <NavLink to={`/mainUser/${item.id}`}>
           <button className="bg-blue-400 px-2 rounded-md">edit</button>
       </NavLink>
-          <button onClick={()=>deleteHandler(user.id)} className="bg-red-400 px-2 rounded-md">delete</button>
+          <button onClick={()=>deleteHandler(item.id)} className="bg-red-400 px-2 rounded-md">delete</button>
 
         </div>
       </td>
