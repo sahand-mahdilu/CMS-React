01736671beb55360256e564464e6c34091b9a4c9
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-02-14 15:21:03 +0330

    feat : recharts pakage installed

--- src/Components/Chart.jsx
+++ src/Components/Chart.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Chart() {
+export default function Chart({title, data, dataKey,grid}) {
   return (
     <div>Chart</div>
   )
---
+++ src/Datas/Datas.js
@@ -0,0 +1,31 @@
+
+let xAxisData=[
+    {"name":"jan",
+    "sale":"121000"},
+    {"name":"feb",
+    "sale":"11000"},
+    {"name":"mar",
+    "sale":"121000"},
+    {"name":"apr",
+    "sale":"221000"},
+    {"name":"may",
+    "sale":"131000"},
+    {"name":"jun",
+    "sale":"141000"},
+    {"name":"jul",
+    "sale":"101000"},
+    {"name":"agu",
+    "sale":"111000"},
+    {"name":"sep",
+    "sale":"151000"},
+    {"name":"oct",
+    "sale":"141000"},
+    {"name":"nov",
+    "sale":"121000"},
+    {"name":"dec",
+    "sale":"151000"},
+    
+    
+
+
+]
