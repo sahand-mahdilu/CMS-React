451d2899df27e581128c0a21dfe86ead406e48f5
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-02-14 20:09:13 +0330

    feat : left sidget completed

--- src/Components/LeftWidget.jsx
+++ src/Components/LeftWidget.jsx
@@ -1,22 +1,41 @@
 import React from 'react'
+import { NewMembers } from '../Datas/NewMembers'
 
 export default function LeftWidget() {
+
+    let members = NewMembers
+    console.log(members);
   return (
-    <div>
+    <div className='w-full'>
         <div>
-            <h2 className='font-bold'>New members</h2>
-            <div>
+            <h2 className='font-bold pb-2'>New members</h2>
+
+            {
+                members.map(member=>{
+
+                    return(
+                        <div className='flex justify-between mt-2'>
                 <div className='img-contaner size-12 rounded-full overflow-hidden'>
                     <img className='w-full h-full' src="./images/profile.png" alt="" />
                 </div>
                 <div className='flex flex-col  items-center'>
-                    <span className='font-semibold'>ali reza</span>
-                    <span className='text-gray-400' >web developer</span>
+                    <span className='font-semibold'>{member.name}</span>
+                    <span className='text-gray-400' >{member.title}</span>
                 </div>
                 <div>
-                    <button className='bg-green-300 px-2 rounded-lg'>approved</button>
+                    <button className={`px-2 rounded-lg ${member.status==="Approved"? "bg-green-300": member.status==="Pending"?"bg-blue-400": "bg-red-400" }`}>{member.status}</button>
                 </div>
             </div>
+
+                    )
+                })
+
+                
+            }
+
+
+
+            
         </div>
 
     </div>
---
+++ src/Datas/NewMembers.js
@@ -0,0 +1,10 @@
+let NewMembers=[
+
+    {id:1,name:"ali reza", title:"web Developer", status:"Approved" },
+    {id:2,name:"ramin", title:"Haker", status:"Declined" },
+    {id:3,name:"farza", title:"SEO eng", status:"Approved" },
+    {id:4,name:"fashid", title:"designer", status:"Pending" },
+]
+
+
+export{NewMembers}
