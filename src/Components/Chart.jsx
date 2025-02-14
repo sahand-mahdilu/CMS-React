5c1fab6fcb7f3a65a0dd17ccd3546e490a9407f5
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-02-14 15:31:39 +0330

    feat : chart content completed

--- src/Components/Chart.jsx
+++ src/Components/Chart.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import {ResponsiveContainer,
+    LineChart,
+    Line,
+    XAxis,
+    CartesianGrid,Tooltip
+} from "recharts"
 
 export default function Chart({title, data, dataKey,grid}) {
   return (
-    <div>Chart</div>
+    <div className='chart'>
+        <h3 className='chartTitle'>
+            {title}
+        </h3>
+
+        <ResponsiveContainer width="100%">
+            <LineChart data={data}>
+
+                <XAxis dataKey="name" stroke='#5550bd'/>
+                <Line dataKey={dataKey} stroke='#5550bd'/>
+                <Tooltip />
+                {grid && <CartesianGrid stroke='#e0dfdf' strokeDasharray={10}/>}
+            </LineChart>
+
+        </ResponsiveContainer>
+
+    </div>
   )
 }
